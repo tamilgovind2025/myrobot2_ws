2d62b6401346b9990c0b7ac9afcdd3e24e07e477
ros <ros@ros.myguest.virtualbox.org>
2024-02-27 11:25:57 +0530

    workspace created for ros

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ros/myrobot2_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ros/serial_ws/devel;/home/ros/tamilgovind2020_ws/devel;/home/ros/pack_ws/devel;/home/ros/brandy_ws/devel;/home/ros/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ros/serial_ws/devel;/home/ros/tamilgovind2020_ws/devel;/home/ros/pack_ws/devel;/home/ros/brandy_ws/devel;/home/ros/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/myrobot2_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/myrobot2_ws/src/myrobot2_examples
+# Build directory: /home/ros/myrobot2_ws/build/myrobot2_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
